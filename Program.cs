4b6220fd8539385b2df3eefc2f62bd4968b92caf
Bruno Torres <brunote16@hotmail.com>
2021-12-01 12:57:49 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+
+namespace MailSyst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            try
+            {
+                MailMessage mail = new MailMessage(); //EMAIL
+                SmtpClient SmtpServer = new SmtpClient("smtp.live.com"); //SERVIDOR
+                mail.From = new MailAddress("brunote16@hotmail.com");
+                var emails = new StreamReader("G:\\Emails\\emails.txt");
+
+                foreach (var email in emails.ReadLine().Split(","))
+                {
+                    mail.To.Add(email); //
+
+                }
+
+                mail.Subject = "TESTE EMAIL"; //
+                mail.IsBodyHtml = true; //
+
+                var body = File.ReadAllText("G:\\Emails\\email.html");
+                body = body.Replace("@Name", "Bruno Torres");
+                body = body.Replace("@Descricao", "Tranquilo demais....");
+
+                mail.Body = body;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("brunote16@hotmail.com", "*");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+
+                Console.WriteLine("SEND MAIL");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+    }
+}
+
